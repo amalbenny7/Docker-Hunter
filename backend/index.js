da02ea555e9850d5b9f0d2a3173443dcb7a36152
Amal Benny <innov8devengine.amalbenny@gmail.com>
2024-08-20 14:43:35 +0000

    Initial commit with all files, including environment and cache files

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const connectToDb = require("./config/db");
+const dotenv = require("dotenv");
+const user = require("./routes/user");
+const issueRoutes = require("./routes/issue");
+
+dotenv.config();
+
+const app = express();
+
+connectToDb();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/user", user);
+
+app.use("/issue", issueRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ status: 200, message: "API Working" });
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on all interfaces at port ${port}`);
+});
+
