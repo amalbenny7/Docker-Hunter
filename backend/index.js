7c010521c1203453963d6528fb72dc42e40bdecd
Amine Anbari <amine.a@wiggli.io>
2024-08-22 20:11:32 +0100

    "Updated backend/index.js: changed static file path, uncommented user and issue routes, and modified catch-all route to send index.html"

--- backend/index.js
+++ backend/index.js
@@ -17,19 +17,20 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
-// app.use("/user", user);
 
-// app.use("/issue", issueRoutes);
+app.use("/user", user);
+
+app.use("/issue", issueRoutes);
 
 // app.get("/", (req, res) => {
-//   res.json({ status: 200, message: "API Working" });
-// });
-
-// app.use("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+  //   res.json({ status: 200, message: "API Working" });
+  // });
+  
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server listening on all interfaces at port ${port}`);
 });
