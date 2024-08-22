ec15b4cf18c188365702a1b869796f156dd97ce1
Amine Anbari <amine.a@wiggli.io>
2024-08-22 17:46:51 +0100

    "Updated static file serving path from './public/dist' to './public'"

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/", express.static(path.join(__dirname, "./public", "dist")));
+app.use("/", express.static(path.join(__dirname, "./publi")));
 
 app.use("/user", user);
 
