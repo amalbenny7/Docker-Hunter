c2f93d7dd833832c65c47b9ce94861e11cfcff16
Amine Anbari <amine.a@wiggli.io>
2024-08-22 19:27:45 +0100

    "Updated static file serving and added catch-all route to serve index.html"

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("*", express.static(path.join(__dirname, "./public")));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use("/user", user);
 
