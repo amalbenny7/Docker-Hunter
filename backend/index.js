7ddb322630d75aa836ab5303bab6e2321209c496
Amine Anbari <amine.a@wiggli.io>
2024-08-22 19:42:30 +0100

    remove the other api routes for test

--- backend/index.js
+++ backend/index.js
@@ -18,18 +18,18 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
-app.use("/user", user);
+// app.use("/user", user);
 
-app.use("/issue", issueRoutes);
+// app.use("/issue", issueRoutes);
 
 // app.get("/", (req, res) => {
 //   res.json({ status: 200, message: "API Working" });
 // });
 
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server listening on all interfaces at port ${port}`);
 });
